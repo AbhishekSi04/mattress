ff3b5f90b83470855ca787a228918b3621cdb9ff
Neelx <indraneelpatil78@gmail.com>
2025-05-06 23:26:38 +0530

    fixing deployment

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ArrowRight, Star, Shield, Truck, Sparkles, Check, Moon } from 'lucide-react';
+import { ArrowRight, Star, Shield, Truck, Check, Moon } from 'lucide-react';
 
 const Hero = () => {
   return (
